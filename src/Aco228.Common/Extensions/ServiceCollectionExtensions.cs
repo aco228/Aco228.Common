959ce6b4a891d4adb46195412ff02b99671598b9
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-01-31 17:15:00 +0100

    dynamic dependency injection

---
+++ src/Aco228.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Aco228.Common.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aco228.Common.Extensions;
+
+public static class DynamicDependencyInjectionExtension
+{
+    public static void RegisterServicesFromAssembly(this IServiceCollection services, Assembly assembly)
+    {
+        var assemblyTypes = assembly.GetTypes();
+        
+        foreach (var assemblyType in assemblyTypes)
+        {
+            if (assemblyType.IsInterface)
+                continue;
+            
+            if(!typeof(IBaseService).IsAssignableFrom(assemblyType))
+                continue;
+
+            var interfaceTypes = assemblyType.GetInterfaces();
+            var inheritInterface = interfaceTypes.FirstOrDefault(x => x.IsInterface);
+            
+            if(typeof(IScoped).IsAssignableFrom(inheritInterface))
+                services.AddScoped(inheritInterface, assemblyType);
+            
+            if(typeof(ITransient).IsAssignableFrom(inheritInterface))
+                services.AddTransient(inheritInterface, assemblyType);
+            
+            if(typeof(ISingleton).IsAssignableFrom(inheritInterface))
+                services.AddSingleton(inheritInterface, assemblyType);
+            
+        }
+    }
+}
---
+++ src/Aco228.Common/Models/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿namespace Aco228.Common.Models;
+
+public interface IBaseService
+{
+    
+}
+
+public interface IScoped : IBaseService { }
+public interface ITransient : IBaseService { }
+public interface ISingleton : IBaseService { }
