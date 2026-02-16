fa4156e45d98d78a3f2bf20415da7f36433fac7f
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-16 19:38:30 +0100

    create theme and offer context

--- src/Aco228.Common/Infrastructure/LiteDb/LiteObject.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Aco228.Common.Infrastructure.LiteDb;
-
-public abstract class LiteObject
-{
-    public virtual string FolderName { get; } = string.Empty;
-    public virtual string FileName { get; } = string.Empty;
-}
