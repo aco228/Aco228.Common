665b9815a7ea4d2c269d38e1375101593901de0c
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-20 01:14:12 +0100

    implement colors in canva

---
+++ src/Aco228.Common/Helpers/DoubleHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Aco228.Common.Helpers;
+
+public static class DoubleHelper
+{
+    public static double GetDifference(double firstValue, double secondValue)
+    {
+        return firstValue - secondValue;
+    }
+    
+    public static double Random(double minValue, double maxValue)
+    {
+        double sample = new Random().NextDouble();
+        return (maxValue * sample) + (minValue * (1d - sample));
+    }
+
+    public static double Multiply(this double input, double multiply)
+        => input * multiply;
+}
---
+++ src/Aco228.Common/Helpers/FloatHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Aco228.Common.Helpers;
+
+public static class FloatHelper
+{
+    public static float GetDifference(float firstValue, float secondValue)
+    {
+        return firstValue - secondValue;
+    }
+
+    public static float Random(float minValue, float maxValue)
+    {
+        float sample = new Random().NextSingle();
+        return (maxValue * sample) + (minValue * (1f - sample));
+    }
+
+    public static float Multiply(this float input, float multiply)
+        => input * multiply;
+}
