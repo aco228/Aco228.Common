6b0f775e2e1d5817d7218acc070c762d62f29fa8
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-03-25 20:00:31 +0100

    clone adset groups

---
+++ src/Aco228.Common/Helpers/PercentageHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Aco228.Common.Helpers;
+
+public static class PercentageHelper
+{
+    public static int AddPercentage(this int input, double percentage)
+    {
+        var change = (int) Math.Ceiling(input * (percentage / 100.0));
+        return input + change;
+    }
+}
