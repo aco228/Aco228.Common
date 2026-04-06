48181cc81b930807142a086bbdcf53bd467e4f18
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-04-07 00:52:45 +0200

    fix killed and unused adsets

---
+++ src/Aco228.Common/Extensions/HashSetExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.IO.Enumeration;
+
+namespace Aco228.Common.Extensions;
+
+public static class HashSetExtensions
+{
+    public static bool ContainsSameElements<T>(this HashSet<T> source, HashSet<T> refer)
+    {
+        return source.SetEquals(refer);
+    }
+}
