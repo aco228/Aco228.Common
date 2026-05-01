c10bcccd9c1f87e17d504535d59f42635fca52a8
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-05-01 15:16:52 +0200

    try kill chrome on zero task

---
+++ src/Aco228.Common/Helpers/CoreKillProcess.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Management;
+
+namespace Aco228.Common.Helpers;
+
+public static class CoreKillProcess
+{
+    public static void TryKill(string processName)
+    {
+        Process[] process = Process.GetProcessesByName(processName);
+        if (process.Length == 0)
+            return;
+        
+        Console.WriteLine($"Killing {process.Length} chrome instances");
+
+        try
+        {
+            foreach (var proc in process)
+                KillProcessAndChildren(proc.Id);
+        }
+        catch
+        {
+            int a = 0;
+        }
+    }
+    
+    public static void KillProcessAndChildren(int pid)
+    {
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessID=" + pid);
+        ManagementObjectCollection moc = searcher.Get();
+        foreach (ManagementObject mo in moc)
+        {
+            KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+        }
+        try
+        {
+            Process proc = Process.GetProcessById(pid);
+            proc.Kill();
+        }
+        catch (ArgumentException)
+        {
+            // Process already exited.
+        }
+    }
+}
