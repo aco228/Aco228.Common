455694cbaf97e5114ee851fb044b2eda7322d3ad
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-06-13 22:58:33 +0200

    connection impl

---
+++ src/Aco228.Common/Helpers/JsonConvertHelper.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Aco228.Common.Helpers;
+
+public static class JsonConvertHelper
+{
+    public static bool TrySerialize<T>(string input, out T output)
+    {
+        output = default;
+        if (string.IsNullOrEmpty(input))
+            return false;
+
+        output = JsonConvert.DeserializeObject<T>(input);
+        return output != null;
+    }
+}
