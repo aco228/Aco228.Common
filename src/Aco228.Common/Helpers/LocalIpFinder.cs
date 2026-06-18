dfdfbd4b2c2ba269140906a4938033acd79a3652
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-06-19 00:01:28 +0200

    emulator prep

---
+++ src/Aco228.Common/Helpers/LocalIpFinder.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Aco228.Common.Helpers;
+
+public static class LocalIpFinder
+{
+    public static string GetLocalIPAddress()
+    {
+        using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+        {
+            socket.Connect("8.8.8.8", 65530);
+            IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+            return endPoint.Address.ToString();
+        }
+    }
+}
