2ebf3f657f9ad55b50d9cb140623c9e3ac329390
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-06-20 22:02:29 +0200

    implement shade ua

--- src/Aco228.Common/Helpers/LocalIpFinder.cs
+++ src/Aco228.Common/Helpers/LocalIpFinder.cs
@@ -7,11 +7,9 @@ public static class LocalIpFinder
 {
     public static string GetLocalIPAddress()
     {
-        using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
-        {
-            socket.Connect("8.8.8.8", 65530);
-            IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
-            return endPoint.Address.ToString();
-        }
+        using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+        socket.Connect("8.8.8.8", 65530);
+        IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+        return endPoint.Address.ToString();
     }
 }
