1091affac096ffc41781484d465e879ea4ab66ef
aco228 <aleksandar.k03@gmail.com>
2026-06-30 11:06:02 +0200

    splico list

--- src/Aco228.Common/Helpers/JsonConvertHelper.cs
+++ src/Aco228.Common/Helpers/JsonConvertHelper.cs
@@ -4,7 +4,19 @@ namespace Aco228.Common.Helpers;
 
 public static class JsonConvertHelper
 {
-    public static bool TrySerialize<T>(string input, out T output)
+    
+    public static bool TryReadFile<T>(string filePath, out T output)
+    {
+        output = default;
+        if (!File.Exists(filePath))
+            return false;
+        
+        var content = File.ReadAllText(filePath);
+        return TrySerialize(content, out output);
+    }
+    
+    
+    public static bool TrySerialize<T>(string input, out T output)  
     {
         output = default;
         if (string.IsNullOrEmpty(input))
