70cd224e40bbccb797ce13a89daf6d715b1d5e82
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-07-03 23:57:47 +0200

    start with yahoo codefuel api service

---
+++ src/Aco228.Common/Models/TimedObject.cs
@@ -0,0 +1,22 @@
+﻿namespace Aco228.Common.Models;
+
+public class TimedObject<T>
+{
+    public T? Data { get; private set; }
+    public DateTime? ValidUntil { get; private set; } = null;
+
+    public T AddOrUpdate(T entry, DateTime? validUntil = null)
+    {
+        Data = entry;
+        ValidUntil = validUntil ?? DateTime.UtcNow.AddMinutes(5);
+        return Data;
+    }
+
+    public T? TryGet()
+    {
+        if (Data == null || ValidUntil == null || ValidUntil < DateTime.UtcNow)
+            return default;
+
+        return Data;
+    }
+}
