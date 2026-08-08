36462b3db9f92a33853effd811cd554d91906b06
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-08-08 20:06:47 +0200

    terms cache logic

---
+++ src/Aco228.Common/Extensions/FileInfoExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Aco228.Common.Extensions;
+
+public static class FileInfoExtensions
+{
+    public static FileInfo Rename(this FileInfo file, string newName, bool overwrite = false)
+    {
+        if (file == null)
+            throw new ArgumentNullException(nameof(file));
+        if (string.IsNullOrWhiteSpace(newName))
+            throw new ArgumentException("New name cannot be null or empty.", nameof(newName));
+
+        string newPath = Path.Combine(file.DirectoryName!, newName);
+        file.MoveTo(newPath, overwrite);
+        return file;
+    } 
+}
