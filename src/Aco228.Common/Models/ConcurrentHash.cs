4d6f6bda5262812531218ef2f8dcda97222441dd
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-08-08 18:19:30 +0200

    shade concurrency issues

---
+++ src/Aco228.Common/Models/ConcurrentHash.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Concurrent;
+
+namespace Aco228.Common.Models;
+
+public class ConcurrentHashSet<T> where T : notnull
+{
+    private readonly ConcurrentDictionary<T, byte> _dict = new();
+
+    public bool Add(T item) => _dict.TryAdd(item, 0);
+    public bool Remove(T item) => _dict.TryRemove(item, out _);
+    public bool Contains(T item) => _dict.ContainsKey(item);
+    public int Count => _dict.Count;
+    public IEnumerable<T> Items => _dict.Keys;
+}
