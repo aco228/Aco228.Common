2dc400ecef9c0da38d3094d63d540f4c1a452b86
aco228 <aleksandar.k03@gmail.com>
2026-08-18 10:22:12 +0200

    fix campaign deleted issue and adset text req serialization

--- src/Aco228.Common/Helpers/JsonConvertHelper.cs
+++ src/Aco228.Common/Helpers/JsonConvertHelper.cs
@@ -6,6 +6,14 @@ namespace Aco228.Common.Helpers;
 public static class JsonConvertHelper
 {
     
+    public static string JsonEscape(this string value)
+    {
+        // Serializes the string as a JSON string literal, then strips the surrounding quotes,
+        // so you get exactly the escaped inner content to drop into your template.
+        var encoded = System.Text.Json.JsonSerializer.Serialize(value);
+        return encoded.Substring(1, encoded.Length - 2);
+    }
+    
     public static bool TryReadFile<T>(string filePath, out T output)
     {
         output = default;
